ee5c64829fc3516f38f3101a5edaa0c06c70aeeb
carlos-paezf <cpaezferreira@gmail.com>
2022-10-10 22:29:34 -0500

    Proyecto base de frontend TS-Redux

--- Segundo_Corte/p2t1-fullstack/frontend/src/App.test.tsx
+++ Segundo_Corte/p2t1-fullstack/frontend/src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import App from './App';
+import { store } from './app/util/store/store';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getByText } = render(
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  );
+
+  // expect(getByText(/learn/i)).toBeInTheDocument();
 });
